d0d84026b208aad56e50791d115bb07c489ebf77
Marco Ferrari <ferrarim@google.com>
2023-01-05 09:20:58 +0000

    Configure the Kubernetes provider version constraint

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -9,6 +9,10 @@ terraform {
       source  = "hashicorp/google-beta"
       version = ">= 4.20.0, <5.0.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0, ~> 2.10"
+    }
     random = {
       source  = "hashicorp/random"
       version = ">= 3.3.1, <4.0.0"
