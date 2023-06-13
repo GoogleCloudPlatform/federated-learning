1df70b813b4c42ea4c122da40ab4e3d094843125
Marco Ferrari <ferrarim@google.com>
2023-06-13 09:54:01 +0000

    Use the Terraform Google Cloud VPC module

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,38 +1,52 @@
-resource "google_compute_network" "vpc" {
-  name                    = "fedlearn-network"
-  project                 = var.project_id
-  auto_create_subnetworks = false
+locals {
+  fedlearn_subnet_name = "subnet-01"
+}
+
+
+module "fedlearn-vpc" {
+  source  = "terraform-google-modules/network/google"
+  version = "7.0.0"
+
+  delete_default_internet_gateway_routes = true
+  project_id                             = var.project_id
+  network_name                           = "fedlearn-network"
+  routing_mode                           = "GLOBAL"
+
+  subnets = [
+    {
+      subnet_name   = local.fedlearn_subnet_name
+      subnet_ip     = "10.2.0.0/16"
+      subnet_region = var.region
+    }
+  ]
+
+  secondary_ranges = {
+    subnet-01 = [
+      {
+        range_name    = "pods"
+        ip_cidr_range = "10.20.0.0/14"
+      },
+      {
+        range_name    = "services"
+        ip_cidr_range = "10.24.0.0/20"
+      },
+    ]
+  }
 
   depends_on = [
     module.project-services
   ]
 }
 
-resource "google_compute_subnetwork" "subnet" {
-  name                     = "subnet-01"
-  ip_cidr_range            = "10.2.0.0/16"
-  region                   = var.region
-  network                  = google_compute_network.vpc.id
-  private_ip_google_access = true
-  secondary_ip_range {
-    range_name    = "pods"
-    ip_cidr_range = "10.20.0.0/14"
-  }
-  secondary_ip_range {
-    range_name    = "services"
-    ip_cidr_range = "10.24.0.0/20"
-  }
-}
-
 resource "google_compute_router" "router" {
   name    = "router"
-  region  = google_compute_subnetwork.subnet.region
-  network = google_compute_network.vpc.id
+  region  = module.fedlearn-vpc.subnets[local.fedlearn_subnet_name].subnet_region
+  network = module.fedlearn-vpc.network_id
 }
 
 resource "google_compute_address" "nat_ip" {
   name   = "nat-manual-ip"
-  region = google_compute_subnetwork.subnet.region
+  region = module.fedlearn-vpc.subnets[local.fedlearn_subnet_name].subnet_region
 }
 
 resource "google_compute_router_nat" "nat" {
@@ -45,7 +59,7 @@ resource "google_compute_router_nat" "nat" {
 
   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
   subnetwork {
-    name                    = google_compute_subnetwork.subnet.id
+    name                    = module.fedlearn-vpc.subnets[local.fedlearn_subnet_name].self_link
     source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
   }
 }
