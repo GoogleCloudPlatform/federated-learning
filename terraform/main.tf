c6e88f9a3a6b9855647bce3db04f79afdc3d7682
Marco Ferrari <ferrarim@google.com>
2023-06-21 13:58:31 +0000

    Move providers config to a dedicated file

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,9 +54,3 @@ data "google_project" "project" {
 }
 
 data "google_client_config" "default" {}
-
-provider "kubernetes" {
-  host                   = "https://${module.gke.endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
-}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,28 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-terraform {
-  required_version = ">=1.0"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 4.20.0, <5.0.0"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = ">= 4.20.0, <5.0.0"
-    }
-    http = {
-      source  = "hashicorp/http"
-      version = ">= 2.2.0, < 4.0.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0, ~> 2.10"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.3.1, <4.0.0"
-    }
-  }
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+provider "google-beta" {
+  project = var.project_id
+  region  = var.region
+}
+
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
 }
