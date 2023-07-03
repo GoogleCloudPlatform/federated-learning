601aae095dcb7a00134044aa039849bcb42be35c
Marco Ferrari <ferrarim@google.com>
2023-07-03 13:42:06 +0300

    Use the Terraform ACM submodule (#181)

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -14,56 +14,26 @@
 
 # See the series of blog posts for details on enabling Anthos Config Management using Terraform
 # https://cloud.google.com/blog/topics/anthos/using-terraform-to-enable-config-sync-on-a-gke-cluster
+module "acm" {
+  source  = "terraform-google-modules/kubernetes-engine/google//modules/acm"
+  version = "27.0.0"
 
-resource "google_gke_hub_membership" "membership" {
-  membership_id = module.gke.name
-  endpoint {
-    gke_cluster {
-      resource_link = "//container.googleapis.com/${module.gke.cluster_id}"
-    }
-  }
-  provider = google-beta
+  project_id   = data.google_project.project.project_id
+  cluster_name = module.gke.name
+  location     = module.gke.location
+
+  configmanagement_version = var.acm_version
+  create_metrics_gcp_sa    = true
+  enable_mutation          = true
+  policy_dir               = var.acm_dir
+  secret_type              = var.acm_secret_type
+  source_format            = "unstructured"
+  sync_repo                = var.acm_repo_location
+  sync_branch              = var.acm_branch
+
 
   depends_on = [
+    module.gke,
     module.project-services
-  ]
-}
-
-resource "google_gke_hub_feature" "feature" {
-  name     = "configmanagement"
-  location = "global"
-  provider = google-beta
-
-  depends_on = [
-    module.project-services
-  ]
-}
-
-resource "google_gke_hub_feature_membership" "feature_member" {
-  location   = "global"
-  feature    = google_gke_hub_feature.feature.name
-  membership = google_gke_hub_membership.membership.membership_id
-  configmanagement {
-    version = var.acm_version
-    config_sync {
-      git {
-        sync_repo   = var.acm_repo_location
-        sync_branch = var.acm_branch
-        policy_dir  = var.acm_dir
-        secret_type = var.acm_secret_type
-      }
-      source_format = "unstructured"
-    }
-    # Note that we enable PolicyController mutations separately below
-    policy_controller {
-      enabled                    = true
-      mutation_enabled           = true
-      template_library_installed = true
-    }
-  }
-  provider = google-beta
-
-  depends_on = [
-    module.asm.asm_wait,
   ]
 }
