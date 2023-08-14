2f3fb49c3d2908b0a777cd42e39c572dd09389c4
Marco Ferrari <ferrarim@google.com>
2023-08-14 10:00:09 +0000

    Fix lock file

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -25,7 +25,7 @@ terraform {
     }
     external = {
       source  = "hashicorp/external"
-      version = ">=2.3.1, < 3.0.0"
+      version = ">=2.2.2, < 3.0.0"
     }
     http = {
       source  = "hashicorp/http"
