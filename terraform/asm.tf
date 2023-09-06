98255a474261d140eb242f5376ae627969edcb21
Marco Ferrari <ferrarim@google.com>
2023-09-06 11:00:57 +0000

    Depend on ASM

--- terraform/asm.tf
+++ terraform/asm.tf
@@ -42,4 +42,8 @@ module "kubectl_asm_wait_for_controlplanerevision" {
   cluster_location        = module.gke.location
   kubectl_create_command  = "kubectl -n istio-system wait ControlPlaneRevision --all --for condition=Reconciled"
   kubectl_destroy_command = ""
+
+  depends_on = [
+    google_gke_hub_feature_membership.mesh_feature_membership
+  ]
 }
