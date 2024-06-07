fc4199cb32ea386f0ab1b1ffe05af1196e237e83
Marco Ferrari <ferrarim@google.com>
2024-06-07 16:11:13 +0200

    ci: configure release-please (#438)

---
+++ scripts/release-please-dry-run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+set -o errexit
+set -o nounset
+
+# shellcheck disable=SC1091,SC1094
+. ./scripts/common.sh
+
+RELEASE_PLEASE_TARGET_BRANCH="${GITHUB_HEAD_REF:-$(git branch --show-current)}"
+
+build_cd_container
+
+check_github_token_file
+
+echo "Running release-please against branch: ${RELEASE_PLEASE_TARGET_BRANCH}"
+docker run \
+  --volume "$(pwd):/source-repository" \
+  "${CD_CONTAINER_URL}" \
+  release-please \
+  release-pr \
+  --config-file .github/release-please/release-please-config.json \
+  --dry-run \
+  --manifest-file .github/release-please/.release-please-manifest.json \
+  --repo-url GoogleCloudPlatform/federated-learning \
+  --target-branch "${RELEASE_PLEASE_TARGET_BRANCH}" \
+  --token "$(cat "$(pwd)/.github-personal-access-token")" \
+  --trace
