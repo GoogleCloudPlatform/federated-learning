a950bd6ac71e691629bd00e64c4f6ee456e37433
Marco Ferrari <ferrarim@google.com>
2024-06-07 13:17:00 +0000

    ci: implement commit message linting
    This is a step towards release automation

---
+++ scripts/lint-commits.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+set -o errexit
+set -o nounset
+
+# shellcheck disable=SC1091,SC1094
+. ./scripts/common.sh
+
+echo "Lint commits"
+
+if [ -z "${FROM_INTERVAL_COMMITLINT:-}" ]; then
+  FROM_INTERVAL_COMMITLINT="HEAD~1"
+fi
+
+if [ -z "${TO_INTERVAL_COMMITLINT:-}" ]; then
+  TO_INTERVAL_COMMITLINT="HEAD"
+fi
+
+build_cd_container
+
+# shellcheck disable=SC2086
+docker run \
+  ${_DOCKER_INTERACTIVE_TTY_OPTION} \
+  -v "$(pwd):/source-repository" \
+  "${CD_CONTAINER_URL}" \
+  commitlint \
+  --config .github/commitlint.config.js \
+  --cwd /source-repository \
+  --from ${FROM_INTERVAL_COMMITLINT} \
+  --to ${TO_INTERVAL_COMMITLINT} \
+  --verbose
